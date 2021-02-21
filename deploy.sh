a872f87c900df7e4cafbb729e1879e023016b039
zaoldyeck <zaoldyeck9970@gmail.com>
2021-02-21 16:46:29 +0800

    Edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
+
 #sbt assembly
-docker build -t zaoldyeck/bitfinex-bot:latest .
+#docker build -t zaoldyeck/bitfinex-bot:latest .
 heroku container:push web -a bitfinex-bot-zaoldyeck
 heroku container:release web -a bitfinex-bot-zaoldyeck
